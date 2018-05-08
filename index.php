71147da7376c7e6a6a5c5f7fdac633e67dab2d33
Akishino1998 <ekopujianto48@gmail.com>
2018-05-08 23:12:42 +0800

    header

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <!DOCTYPE html>
 <html>
-<link rel="stylesheet" type="text/css" href="css/modif.css">
+
 <head>
 	<title></title>
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/modif.css">
+    <link rel="stylesheet" type="text/css" href="css/materialize.min.css ">
+    <meta name="viewreport" content="width=device-width, intial-scale=1.0">
 </head>
 <body>
-	<div id="header">
-		<h1>Doctor Computer</h1>
-		<header>
-			<ul id="menu1">
-				<li><a href="facebook.com/ekopujianto1998" class="menu2">DC</a></li>
-			</ul>
-		</header>
-	</div>
+	<?php include("layout/header.php") ?>
 
+	
+	<script src="js/materialize.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </body>
 </html>
