7c7b3767f0b6bab33174d290527b334f6a577546
egimel <egirajasa@gmail.com>
2018-05-08 22:19:26 +0800

    update by egi

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<link rel="stylesheet" type="text/css" href="modif.css">
+<head>
+	<title></title>
+</head>
+<body>
+		<nav>
+			<a href="Menu 1">Menu 1</a>
+			<a href="Menu 2">Menu 2</a>
+			<a href="Menu 3">Menu 3</a>
+		</nav>
+</body>
+</html>
