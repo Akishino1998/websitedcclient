760e9968d6c679cdc78935e41a398b983d20b483
Akishino1998 <ekopujianto48@gmail.com>
2018-05-08 23:24:28 +0800

    header

--- index.php
+++ index.php
@@ -9,20 +9,7 @@
     <meta name="viewreport" content="width=device-width, intial-scale=1.0">
 </head>
 <body>
-<<<<<<< HEAD
-	<div id="header">
-		<h1>Doctor Computer</h1>
-		<header>
-			<ul id="menu1">
-				<li><a href="" class="menu2">DC</a></li>
-				<li><a href="">"Menu3"</a>NGENTOD	</li>
-			</ul>
-		</header>
-	</div>
-=======
 	<?php include("layout/header.php") ?>
->>>>>>> 8b156b056e65ffb97b6b40fcfd421e568764d35a
-
 	
 	<script src="js/materialize.min.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
