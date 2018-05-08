57b27b48703b308adf33316ef54869989ead8fc4
Akishino1998 <ekopujianto48@gmail.com>
2018-05-08 23:38:31 +0800

    update header

--- layout/header.php
+++ layout/header.php
@@ -1,6 +1,6 @@
-<nav>
-	<div class="nav-wrapper">
-		<a href="#" class="brand-logo">Doctor Computer</a>
+<nav class="navbar-fixed">
+	<div class="nav-wrapper light-blue lighten-1">
+		<a href="#" class="brand-logo dc_head">Doctor Computer</a>
 		<ul class="right">
 			<li><a href="#">MENU1</a></li>
 			<li><a href="#">MENU2</a></li>
