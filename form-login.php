85c79e8a871faaaeada2db8896991d5535b1a701
AlMuttaqin <rizabreakerz@gmail.com>
2018-05-14 18:53:17 +0800

    login

--- form-login.php
+++ form-login.php
@@ -4,7 +4,7 @@
 	<title>Login | Doctor Computer</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->	
+<!--===============================================================================================-->
 	<link rel="icon" type="image/png" href="img/icons/favicon.ico"/>
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
@@ -12,7 +12,7 @@
 	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
-<!--===============================================================================================-->	
+<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
@@ -30,12 +30,12 @@
 					<img src="img/logo.png" alt="IMG">
 				</div>
 
-				<form class="login100-form validate-form">
+			<form method="get" action="form-login-process.php">
 					<span class="login100-form-title">
 						Login
 					</span>
 
-					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: drcomputer@example.com">
+					<div class="wrap-input100 validate-input" data-validate = "Contoh Email : drcomputer@example.com">
 						<input class="input100" type="text" name="email" placeholder="Email">
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
@@ -43,14 +43,14 @@
 						</span>
 					</div>
 
-					<div class="wrap-input100 validate-input" data-validate = "Password is required">
+					<div class="wrap-input100 validate-input" data-validate = "Password dibutuhkan">
 						<input class="input100" type="password" name="pass" placeholder="Password">
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
 							<i class="fa fa-lock" aria-hidden="true"></i>
 						</span>
 					</div>
-					
+
 					<div class="container-login100-form-btn">
 						<button class="login100-form-btn">
 							Login
@@ -66,16 +66,16 @@
 						</a>
 					</div>
 
-					
+
 				</form>
 			</div>
 		</div>
 	</div>
-	
-	
 
-	
-<!--===============================================================================================-->	
+
+
+
+<!--===============================================================================================-->
 	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
 <!--===============================================================================================-->
 	<script src="vendor/bootstrap/js/popper.js"></script>
